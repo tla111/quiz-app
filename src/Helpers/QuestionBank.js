73e7afed7016e3b64761e55101f661394dfefeaa
Tim La <email@example.com>
2021-02-23 10:23:27 -0500

    question bank has three questions

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+  {
+    prompt: 'What is 1+3?',
+    optionA: '24',
+    optionB: '21',
+    optionC: '4',
+    optionD: '2',
+    answer: 'optionC',
+  },
+  {
+    prompt: 'Which of this is not a programming language?',
+    optionA: 'Python',
+    optionB: 'JavaScript',
+    optionC: 'MC-03',
+    optionD: 'Java',
+    answer: 'optionC',
+  },
+  {
+    prompt: 'Which of this is not a javascript framework?',
+    optionA: 'React',
+    optionB: 'Angular',
+    optionC: 'Vue',
+    optionD: 'Java',
+    answer: 'optionD',
+  },
+];
